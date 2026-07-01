549841a406ab40895c253149e963cc4e49b7e254
atadityas13 <at.adityas13@gmail.com>
2026-07-01 11:20:11 +0700

    fix: remove dummy teachers Dede Apip and Maman Supratman from seeders

--- database/seeders/GuruMapelsTableSeeder.php
+++ database/seeders/GuruMapelsTableSeeder.php
@@ -18,25 +18,6 @@ class GuruMapelsTableSeeder extends Seeder
 
         \DB::table('guru_mapels')->delete();
         
-        \DB::table('guru_mapels')->insert(array (
-            0 => 
-            array (
-                'id' => 1,
-                'guru_id' => 1,
-                'mapel_id' => 2,
-                'created_at' => NULL,
-                'updated_at' => NULL,
-            ),
-            1 => 
-            array (
-                'id' => 3,
-                'guru_id' => 2,
-                'mapel_id' => 6,
-                'created_at' => NULL,
-                'updated_at' => NULL,
-            ),
-        ));
-        
         
     }
 }
--- database/seeders/GurusTableSeeder.php
+++ database/seeders/GurusTableSeeder.php
@@ -18,51 +18,6 @@ class GurusTableSeeder extends Seeder
 
         \DB::table('gurus')->delete();
         
-        \DB::table('gurus')->insert(array (
-            0 => 
-            array (
-                'id' => 1,
-                'username' => '196801171992031002',
-                'kode_guru' => 'DA',
-                'duk' => 1,
-                'status_pegawai' => 'PNS',
-                'nama_guru' => 'H. DEDE APIP MUSTOPA',
-                'gelar_depan' => NULL,
-                'gelar_belakang' => 'S.Ag.',
-                'nuptk' => '91000068110144',
-                'jabatan' => 'Ahli Madya - Guru Akidah Akhlak',
-                'golongan' => 'IV/a',
-                'status_sertifikasi' => 1,
-                'is_bk' => 0,
-                'mapel_ijazah_id' => NULL,
-                'rumpun_ijazah_id' => 1,
-                'mapel_sertifikasi_id' => 2,
-                'created_at' => '2026-04-13 02:22:51',
-                'updated_at' => '2026-04-13 02:22:51',
-            ),
-            1 => 
-            array (
-                'id' => 2,
-                'username' => '197112282023211002',
-                'kode_guru' => 'MS',
-                'duk' => 2,
-                'status_pegawai' => 'PPPK',
-                'nama_guru' => 'MAMAN SUPRATMAN',
-                'gelar_depan' => NULL,
-                'gelar_belakang' => 'S.Sos.',
-                'nuptk' => '91000071125588',
-                'jabatan' => 'Ahli Pertama - Guru PKN',
-                'golongan' => 'IX',
-                'status_sertifikasi' => 1,
-                'is_bk' => 0,
-                'mapel_ijazah_id' => 6,
-                'rumpun_ijazah_id' => NULL,
-                'mapel_sertifikasi_id' => 6,
-                'created_at' => '2026-04-13 03:03:34',
-                'updated_at' => '2026-07-01 02:08:12',
-            ),
-        ));
-        
         
     }
 }
